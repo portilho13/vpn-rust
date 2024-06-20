a887e3b152a672abd8bd852845a26320d18f38f4
portilho13 <junior.portilho2005@gmail.com>
2024-06-20 17:49:43 +0100

    Basic tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod args;
+mod server;
 
 fn main() {
     let args = args::get_args();
-    
+
     let mode = args[0].to_string();
     let ip = args[1].to_string();
+
+    if mode == "server" {
+        server::server(ip)
+    }
 
 }
---
+++ src/server.rs
@@ -0,0 +1,27 @@
+use std::{io, net::TcpListener};
+
+pub fn server(ip: String) {
+    let listener = match create_listener(ip) {
+        Ok(t) => t,
+        Err(err) => {
+            println!("Error: {}", err); 
+            return
+        }
+    };
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("Client with IP: {} connected sucessfully", stream.peer_addr().unwrap());
+            }
+            Err (err) => {
+                println!("Connection failed: {}", err)
+            }
+        }
+    }
+}
+
+fn create_listener(ip: String) -> Result<TcpListener, io::Error> {
+    let listener = TcpListener::bind(ip)?;
+    Ok(listener)
+}
